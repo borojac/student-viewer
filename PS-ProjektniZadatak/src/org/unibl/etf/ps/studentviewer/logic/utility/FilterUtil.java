24b3007aeb75e2adf609eb70174fa933a3aef37e
borojac <borojac3@gmail.com>
2017-09-13 22:11:47 +0100

    Popravljen Filter.

--- PS-ProjektniZadatak/src/org/unibl/etf/ps/studentviewer/logic/utility/FilterUtil.java
+++ PS-ProjektniZadatak/src/org/unibl/etf/ps/studentviewer/logic/utility/FilterUtil.java
@@ -4,35 +4,38 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
 
+import javax.swing.JOptionPane;
+
 import org.unibl.etf.ps.studentviewer.logic.controller.MainFormController;
 import org.unibl.etf.ps.studentviewer.model.dto.StudentMainTableDTO;
 
 public class FilterUtil {
 
-	public static ArrayList<StudentMainTableDTO> filter(MainFormController mainFormController, HashMap<String, HashMap<String, String>> testoviMap, ArrayList<String> controlParams) {
+	public static ArrayList<StudentMainTableDTO> filter(MainFormController mainFormController,
+			HashMap<String, HashMap<String, String>> testoviMap, ArrayList<String> controlParams) {
 		ArrayList<StudentMainTableDTO> filteredStudents = new ArrayList<StudentMainTableDTO>();
 		ArrayList<StudentMainTableDTO> currentStudents = mainFormController.getMainTable().getStudents();
-		
+
 		boolean ideNaElektrijadu = controlParams.contains(Filter.IDE_NA_ELEKTRIJADU);
 		boolean neIdeNaElektrijadu = controlParams.contains(Filter.NE_IDE_NA_ELEKTRIJADU);
 		boolean imaKomentar = controlParams.contains(Filter.IMA_KOMENTAR);
 		boolean nemaKomentar = controlParams.contains(Filter.NEMA_KOMENTAR);
-		
+
 		Set<String> testovi = testoviMap.keySet();
-		
+
 		for (StudentMainTableDTO s : currentStudents) {
 			if (ideNaElektrijadu)
 				if ("NE".equals(s.getElektrijada()))
 					continue;
-			
+
 			if (neIdeNaElektrijadu)
 				if ("DA".equals(s.getElektrijada()))
 					continue;
-			
+
 			if (imaKomentar)
 				if ("".equals(s.getKomentar()))
 					continue;
-			
+
 			if (nemaKomentar)
 				if (!"".equals(s.getKomentar()))
 					continue;
@@ -45,50 +48,54 @@ public class FilterUtil {
 				boolean nijeIzasao = "true".equals(testoviMap.get(test).get(Filter.NIJE_IZASAO));
 				Integer viseOd = new Integer(testoviMap.get(test).get(Filter.VISE_OD));
 				Integer manjeOd = new Integer(testoviMap.get(test).get(Filter.MANJE_OD));
-				
+
 				if (izasao)
 					if (s.getTest(test) == null) {
 						breakControl = true;
 						break;
 					}
-				
+
 				if (nijeIzasao)
 					if (s.getTest(test) != null) {
 						breakControl = true;
 						break;
 					}
-				
+
 				if (polozen)
 					if (s.getTest(test) != null && new Integer(s.getTest(test)) <= 50) {
 						breakControl = true;
 						break;
 					}
-				
+
 				if (nijePolozen)
 					if (s.getTest(test) != null && new Integer(s.getTest(test)) > 50) {
 						breakControl = true;
 						break;
 					}
-				
+
 				Integer poeni = new Integer(s.getTest(test));
 				if (viseOd > poeni) {
 					breakControl = true;
 					break;
 				}
-				
+
 				if (manjeOd < poeni) {
 					breakControl = true;
 					break;
 				}
-				
+
 			}
 			if (breakControl)
 				continue;
-			
+
 			filteredStudents.add(s);
 		}
-		
+
+		if (filteredStudents.size() == 0) {
+			JOptionPane.showMessageDialog(null, "Nema odgovarajucih rezultata! Nijedna promjena na tabeli nije izvrsena.");
+			filteredStudents = currentStudents;
+		}
 		return filteredStudents;
 	}
-	
+
 }
