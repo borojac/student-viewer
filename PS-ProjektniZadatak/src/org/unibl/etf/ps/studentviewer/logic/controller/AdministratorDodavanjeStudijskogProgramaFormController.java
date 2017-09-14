2c6589b0d0e514ee3e2b7ad33be7b4288368f3c9
Dejan <Dejan@DESKTOP-J7J6KGN>
2017-09-14 23:54:05 +0200

    Izmjene u kodu.

---
+++ PS-ProjektniZadatak/src/org/unibl/etf/ps/studentviewer/logic/controller/AdministratorDodavanjeStudijskogProgramaFormController.java
@@ -0,0 +1,17 @@
+package org.unibl.etf.ps.studentviewer.logic.controller;
+
+import org.unibl.etf.ps.studentviewer.gui.view.AdministratorDodavanjeStudijskogProgramaForm;
+
+public class AdministratorDodavanjeStudijskogProgramaFormController {
+	
+	private AdministratorDodavanjeStudijskogProgramaForm administratorDodavanjeStudijskogProgramaForm;
+	
+	public AdministratorDodavanjeStudijskogProgramaFormController(AdministratorDodavanjeStudijskogProgramaForm administratorDodavanjeStudijskogProgramaForm) {
+		this.administratorDodavanjeStudijskogProgramaForm = administratorDodavanjeStudijskogProgramaForm;
+	}
+	
+	public void dodajStudProg() {
+		
+	}
+
+}
